9155fa74697c59fc51e0eb47ab5791aec26e93e9
joctan-maceda <123424180+joctan-maceda@users.noreply.github.com>
2024-02-22 10:15:48 -0600

    Se agregaron la pagina de presentacion antes de hacer las preguntas

--- router.js
+++ router.js
@@ -9,6 +9,10 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.get('/presentacion', (req, res) => {
+    res.render('presentacion');
+})
+
 router.get('/pregunta', (req, res) => res.render('pregunta', { pregunta: preguntaActual.pregunta}) );
 
 router.post('/respuesta', (req, res) => {
